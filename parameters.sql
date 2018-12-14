081846ecf3e848aa85905ad2a414d3346f73b777
Vitor Castro <v.j.r.c1997@hotmail.com>
2018-12-14 09:55:13 +0000

    QUERY Tablespaces info

--- parameters.sql
+++ parameters.sql
@@ -26,8 +26,25 @@
 --V$SYS_TIME_MODEL
 --V$SESS_TIME_MODEL
 
-
-SELECT * FROM V$SESSTAT;
-
-
-SELECT * FROM DBA_TABLESPACES;
+-- TABLESPACES INFO
+SELECT 
+   ts.tablespace_name, "File Count",
+   TRUNC("SIZE(MB)", 2) "Size(MB)",
+   TRUNC(fr."FREE(MB)", 2) "Free(MB)",
+   TRUNC("SIZE(MB)" - "FREE(MB)", 2) "Used(MB)",
+   df."MAX_EXT" "Max Ext(MB)",
+   (fr."FREE(MB)" / df."SIZE(MB)") * 100 "% Free"
+FROM 
+   (SELECT tablespace_name,
+   SUM (bytes) / (1024 * 1024) "FREE(MB)"
+   FROM dba_free_space
+    GROUP BY tablespace_name) fr,
+(SELECT tablespace_name, SUM(bytes) / (1024 * 1024) "SIZE(MB)", COUNT(*)
+"File Count", SUM(maxbytes) / (1024 * 1024) "MAX_EXT"
+FROM dba_data_files
+GROUP BY tablespace_name) df,
+(SELECT tablespace_name
+FROM dba_tablespaces) ts
+WHERE fr.tablespace_name = df.tablespace_name (+)
+AND fr.tablespace_name = ts.tablespace_name (+)
+ORDER BY "% Free" desc;
