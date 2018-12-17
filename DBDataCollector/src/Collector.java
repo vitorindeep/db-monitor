d1df162a8f61cae4d364c2d8df27e3f7527dd758
Vitor Castro <v.j.r.c1997@hotmail.com>
2018-12-17 19:18:03 +0000

    COLLECTOR all done up to SGA

--- DBDataCollector/src/Collector.java
+++ DBDataCollector/src/Collector.java
@@ -1,13 +1,6 @@
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 import java.lang.String ;
-import java.sql.PreparedStatement;
-import java.sql.Timestamp;
 import java.util.Date ;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -64,27 +57,27 @@ public class Collector  {
                         "   TRUNC(\"SIZE(MB)\" - \"FREE(MB)\", 2) \"Used(MB)\"," +
                         "   df.\"MAX_EXT\" \"Max Ext(MB)\"," +
                         "   (fr.\"FREE(MB)\" / df.\"SIZE(MB)\") * 100 \"% Free\"" +
-                        "FROM " +
+                        "   FROM " +
                         "   (SELECT tablespace_name," +
                         "   SUM (bytes) / (1024 * 1024) \"FREE(MB)\"" +
                         "   FROM dba_free_space" +
-                        "    GROUP BY tablespace_name) fr," +
-                        "(SELECT tablespace_name, SUM(bytes) / (1024 * 1024) \"SIZE(MB)\", COUNT(*)" +
-                        "\"File Count\", SUM(maxbytes) / (1024 * 1024) \"MAX_EXT\"" +
-                        "FROM dba_data_files" +
-                        "GROUP BY tablespace_name) df," +
-                        "(SELECT tablespace_name" +
-                        "FROM dba_tablespaces) ts" +
-                        "WHERE fr.tablespace_name = df.tablespace_name (+)" +
-                        "AND fr.tablespace_name = ts.tablespace_name (+)" +
-                        "ORDER BY \"% Free\" desc;";
+                        "   GROUP BY tablespace_name) fr," +
+                        "   (SELECT tablespace_name, SUM(bytes) / (1024 * 1024) \"SIZE(MB)\", COUNT(*)" +
+                        "   \"File Count\", SUM(maxbytes) / (1024 * 1024) \"MAX_EXT\"" +
+                        "   FROM dba_data_files" +
+                        "   GROUP BY tablespace_name) df," +
+                        "   (SELECT tablespace_name" +
+                        "   FROM dba_tablespaces) ts" +
+                        "   WHERE fr.tablespace_name = df.tablespace_name (+)" +
+                        "   AND fr.tablespace_name = ts.tablespace_name (+)" +
+                        "   ORDER BY \"% Free\" desc;";
                 Statement getStmt = sysConn.createStatement();
                 ResultSet resultSet = getStmt.executeQuery(getTablespaces);
 
                 while(resultSet.next()) {
 
                     Statement monitorStmt = monitorConn.createStatement();
-                    String updateQuery = "UPDATE \"MONITOR.TABLESPACES\" " +
+                    String updateQuery = "UPDATE \"MONITOR\".\"TABLESPACES\" " +
                             " SET filecount = " + Float.parseFloat(resultSet.getString("File Count")) +
                             "," + " size = " + resultSet.getString("Size(MB)") +
                             "," + " free = " + resultSet.getString("Free(MB)") +
@@ -120,59 +113,45 @@ public class Collector  {
 
 
                 // DATAFILES
-                String getDatafiles = "SELECT Substr(df.file_name,1,500) NAME_DF, " +
-                        "Substr(df.tablespace_name,1,40) NAME_TB, " +
-                        "Round(df.bytes/1024/1024,0) FILE_SIZE, " +
-                        "decode(e.used_bytes,NULL,0,Round(e.used_bytes/1024/1024,0)) USED_SIZE, " +
-                        "decode(f.free_bytes,NULL,0,Round(f.free_bytes/1024/1024,0)) FREE_SIZE, " +
-                        "c.CURRENT_TIMESTAMP TIMESTAMP, " +
-                        "d.CURRENT_TIMESTAMP TIMESTAMP_FK " +
-                        " FROM DBA_DATA_FILES DF, " +
-                        " (SELECT file_id, " +
-                        " Sum(Decode(bytes,NULL,0,bytes)) used_bytes " +
-                        " FROM dba_extents " +
-                        " GROUP by file_id) E, " +
-                        " (SELECT Max(bytes) free_bytes, file_id " +
-                        " FROM dba_free_space " +
-                        " GROUP BY file_id) f, " +
-                        " (SELECT CURRENT_TIMESTAMP FROM dual) c, " +
-                        " (SELECT CURRENT_TIMESTAMP FROM dual) d " +
-                        " WHERE e.file_id (+) = df.file_id " +
-                        " AND df.file_id = f.file_id (+) " +
-                        " ORDER BY df.tablespace_name,df.file_name" ;
-
+                String getDatafiles = "SELECT file_name, file_id, tablespace_name, bytes, blocks, status, autoextensible, maxbytes, maxblocks, online_status" +
+                                        " FROM dba_data_files;" ;
                 resultSet = getStmt.executeQuery(getDatafiles);
                 while(resultSet.next()) {
 
                     Statement stmt1 = monitorConn.createStatement();
 
-                    String tbs1 = "UPDATE DATAFILE " +
-                            " SET USED_SIZE = " + resultSet.getString("USED_SIZE") +
-                            "," + " FILE_SIZE = " + resultSet.getString("FILE_SIZE") +
-                            "," + " FREE_SIZE = " + resultSet.getString("FREE_SIZE") +
-                            "," + " TIMESTAMP = CURRENT_TIMESTAMP" +
-                            " WHERE NAME_DF = " + "'" + resultSet.getString("NAME_DF") + "'";
+                    String updateDatafiles = "UPDATE \"MONITOR\".\"DATAFILES\" " +
+                            " SET bytes = " + Float.parseFloat(resultSet.getString("BYTES")) +
+                            "," + " blocks = " + Float.parseFloat(resultSet.getString("BLOCKS")) +
+                            "," + " status = " + resultSet.getString("STATUS") +
+                            "," + " autoextensible = " + resultSet.getString("AUTOEXTENSIBLE") +
+                            "," + " maxbytes = " + Float.parseFloat(resultSet.getString("MAXBYTES")) +
+                            "," + " maxblocks = " + Float.parseFloat(resultSet.getString("MAXBLOCKS")) +
+                            "," + " onlinestatus = " + resultSet.getString("ONLINE_STATUS") +
+                            "," + " timestamp = CURRENT_TIMESTAMP" +
+                            " WHERE filename = " + "'" + resultSet.getString("FILE_NAME") + "'";
 
                     int i;
-                    i = stmt1.executeUpdate(tbs1);
+                    i = stmt1.executeUpdate(updateDatafiles);
 
                     if(i==0) {
 
-                        getDatafiles = "INSERT INTO DATAFILE (NAME_DF,NAME_TB,FILE_SIZE,USED_SIZE,FREE_SIZE,TIMESTAMP) "
-                                + "VALUES(?, ?, ?, ?, ?, CURRENT_TIMESTAMP)" ;
-                        psmt = monitorConn.prepareStatement(getDatafiles) ;
+                        String insertDatafiles = "INSERT INTO \"MONITOR\".\"DATAFILES\" "
+                                + "(filename,fileid,tablename,bytes,blocks,status,autoextensible,maxbytes,maxblocks,onlinestatus,timestamp) "
+                                + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)" ;
+                        psmt = monitorConn.prepareStatement(insertDatafiles) ;
 
 
-                        psmt.setString(1,resultSet.getString("NAME_DF")) ;
-                        psmt.setString(2,resultSet.getString("NAME_TB")) ;
-                        psmt.setFloat(3,Float.parseFloat(resultSet.getString("FILE_SIZE"))) ;
-                        if(resultSet.getString("USED_SIZE") != null )
-                            psmt.setFloat(4,Float.parseFloat(resultSet.getString("USED_SIZE"))) ;
-                        else
-                            psmt.setNull(4,java.sql.Types.NUMERIC);
-                        if(resultSet.getString("FREE_SIZE") != null )
-                            psmt.setFloat(5,Float.parseFloat(resultSet.getString("FREE_SIZE"))) ;
-                        else psmt.setNull(5,java.sql.Types.NUMERIC);
+                        psmt.setString(1,resultSet.getString("FILE_NAME")) ;
+                        psmt.setFloat(2,Float.parseFloat(resultSet.getString("FILE_ID"))) ;
+                        psmt.setString(3,resultSet.getString("TABLESPACE_NAME")) ;
+                        psmt.setFloat(4,Float.parseFloat(resultSet.getString("BYTES"))) ;
+                        psmt.setFloat(5,Float.parseFloat(resultSet.getString("BLOCKS"))) ;
+                        psmt.setString(6,resultSet.getString("STATUS")) ;
+                        psmt.setString(7,resultSet.getString("AUTOEXTENSIBLE")) ;
+                        psmt.setFloat(8,Float.parseFloat(resultSet.getString("MAXBYTES"))) ;
+                        psmt.setFloat(9,Float.parseFloat(resultSet.getString("MAXBLOCKS"))) ;
+                        psmt.setString(10,resultSet.getString("ONLINE_STATUS")) ;
                         psmt.executeUpdate();
                         psmt.close() ;
                     }
@@ -181,47 +160,56 @@ public class Collector  {
 
 
                 // USERS
-
-                String users = "select USER_ID,USERNAME,ACCOUNT_STATUS,"+
-                        " DEFAULT_TABLESPACE DEFAULT_TB,TEMPORARY_TABLESPACE TEMP_TB,CREATED,c.CURRENT_TIMESTAMP " +
-                        " from dba_users, " +
-                        " (SELECT CURRENT_TIMESTAMP FROM dual) c " +
-                        " where ACCOUNT_STATUS = 'OPEN' "+
-                        " order by 1" ;
+                String users = "SELECT USERNAME, ACCOUNT_STATUS, COMMON, EXPIRY_DATE, DEFAULT_TABLESPACE, TEMPORARY_TABLESPACE, PROFILE, CREATED" +
+                        " FROM dba_users;";
                 resultSet = getStmt.executeQuery(users);
 
                 while(resultSet.next()) {
 
-                    Statement stmt3 = monitorConn.createStatement();
-                    String tbs3 = " UPDATE USERS" +
-                            " SET ACCOUNT_STATUS = "+"'"+resultSet.getString("ACCOUNT_STATUS")+"'"+
-                            "," + " TIMESTAMP = CURRENT_TIMESTAMP "  +
-                            " WHERE USER_ID = "  + resultSet.getString("USER_ID") ;
+                    Statement stmtUsers = monitorConn.createStatement();
+                    String updateUser = " UPDATE \"MONITOR\".\"USERS\"" +
+                            " SET accStatus = " + "'" + resultSet.getString("ACCOUNT_STATUS")+"'"+
+                            "," + " common = " + resultSet.getString("COMMON") +
+                            "," + " defaultTablespace = " + resultSet.getString("DEFAULT_TABLESPACE") +
+                            "," + " tempTablespace = " + resultSet.getString("TEMPORARY_TABLESPACE") +
+                            "," + " profile = " + resultSet.getString("PROFILE") +
+                            "," + " timestamp = CURRENT_TIMESTAMP "  +
+                            " WHERE username = "  + resultSet.getString("USERNAME") ;
+
                     int i=0;
-                    i = stmt3.executeUpdate(tbs3);
+                    i = stmtUsers.executeUpdate(updateUser);
+
                     if(i==0) {
-                        users = "INSERT INTO USERS (USER_ID,USERNAME,ACCOUNT_STATUS,TEMP_TB,CREATED,TIMESTAMP,NAME_TB) "
-                                + "VALUES(?, ?, ?, ?, ?, CURRENT_TIMESTAMP,?)" ;
+
+                        users = "INSERT INTO \"MONITOR\".\"USERS\""
+                                + " (username,accStatus,common,expiryDate,defaultTablespace,tempTablespace,profile,created,timestamp)"
+                                + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP,?)" ;
                         psmt = monitorConn.prepareStatement(users) ;
 
-                        psmt.setFloat(1,Float.parseFloat(resultSet.getString("USER_ID"))) ;
-                        psmt.setString(2,resultSet.getString("USERNAME")) ;
-                        psmt.setString(3,resultSet.getString("ACCOUNT_STATUS")) ;
-                        psmt.setString(4,resultSet.getString("TEMP_TB")) ;
-                        psmt.setDate(5,resultSet.getDate("CREATED")) ;
-                        psmt.setString(6,resultSet.getString("DEFAULT_TB"));
+                        psmt.setString(1,resultSet.getString("USERNAME")) ;
+                        psmt.setString(2,resultSet.getString("ACCOUNT_STATUS")) ;
+                        psmt.setString(3,resultSet.getString("COMMON")) ;
+                        // caso não tenha expiry date
+                        if(resultSet.getDate("EXPIRY_DATE") != null )
+                            psmt.setDate(4,resultSet.getDate("EXPIRY_DATE")) ;
+                        else
+                            psmt.setNull(4, Types.TIMESTAMP);
+                        psmt.setString(5,resultSet.getString("DEFAULT_TABLESPACE")) ;
+                        psmt.setString(6,resultSet.getString("TEMPORARY_TABLESPACE")) ;
+                        psmt.setString(7,resultSet.getString("PROFILE")) ;
+                        psmt.setDate(8,resultSet.getDate("CREATED")) ;
+                        
                         psmt.executeUpdate();
                         psmt.close() ;
                     }
                 }
 
-                // SESSIONS
-
-                String ses = "select SID, USER# USER_ID, USERNAME, SERIAL# SERIAL ,c.CURRENT_TIMESTAMP TIMESTAMP" +
+                // SGA
+                String sga = "select SID, USER# USER_ID, USERNAME, SERIAL# SERIAL ,c.CURRENT_TIMESTAMP TIMESTAMP" +
                         " from v$session, (SELECT CURRENT_TIMESTAMP FROM dual) c " + " WHERE USERNAME IS NOT NULL" +
                         " order by 1" ;
 
-                resultSet = getStmt.executeQuery(ses);
+                resultSet = getStmt.executeQuery(sga);
 
                 while(resultSet.next()) {
                     if(!resultSet.getString("USERNAME").equals("MANAGER")) {
@@ -238,9 +226,9 @@ public class Collector  {
 
                         if(i==0) {
 
-                            ses = "INSERT INTO SESSIONS (SID,USER_ID,USERNAME,SERIAL,TIMESTAMP)"
+                            sga = "INSERT INTO SESSIONS (SID,USER_ID,USERNAME,SERIAL,TIMESTAMP)"
                                     + "VALUES(?, ?, ?, ?, CURRENT_TIMESTAMP)" ;
-                            psmt = monitorConn.prepareStatement(ses) ;
+                            psmt = monitorConn.prepareStatement(sga) ;
 
                             psmt.setString(1,resultSet.getString("SID")) ;
                             psmt.setFloat(2,Float.parseFloat(resultSet.getString("USER_ID")));
