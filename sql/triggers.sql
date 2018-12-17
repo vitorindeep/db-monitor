6ba9114b0190e1333513f3258fe1de6af4bf7f4e
Vitor Castro <v.j.r.c1997@hotmail.com>
2018-12-17 22:40:01 +0000

    TRIGGERS fixing calls

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,29 +1,29 @@
 -- SGA HISTORIC
 CREATE OR REPLACE TRIGGER add_to_SGA_HIST
-AFTER UPDATE ON "MONITOR.SGA" FOR EACH ROW
+AFTER UPDATE ON "MONITOR"."SGA" FOR EACH ROW
 BEGIN
-    INSERT INTO "MONITOR.SGA_HIST"
-        (name,total,timestamp)
+    INSERT INTO "MONITOR"."SGA_HIST"
+        ("name","total","timestamp")
     VALUES
-        (:OLD.name,:OLD.total,:OLD.timestamp);
+        (:OLD."name",:OLD."total",:OLD."timestamp");
 END;
 
 -- PGA HISTORIC
 CREATE OR REPLACE TRIGGER add_to_PGA_HIST
-AFTER UPDATE ON "MONITOR.PGA" FOR EACH ROW
+AFTER UPDATE ON "MONITOR"."PGA" FOR EACH ROW
 BEGIN
-    INSERT INTO "MONITOR.PGA_HIST"
-        (usedPga,timestamp)
+    INSERT INTO "MONITOR"."PGA_HIST"
+        ("usedPga","timestamp")
     VALUES
-        (:OLD.usedPga,:OLD.timestamp);
+        (:OLD."usedPga",:OLD."timestamp");
 END;
 
 -- CPU HISTORIC
 CREATE OR REPLACE TRIGGER add_to_CPU_HIST
-AFTER UPDATE ON "MONITOR.CPU" FOR EACH ROW
+AFTER UPDATE ON "MONITOR"."CPU" FOR EACH ROW
 BEGIN
-    INSERT INTO "MONITOR.CPU_HIST"
-        (username,cpuUsage,timestamp)
+    INSERT INTO "MONITOR"."CPU_HIST"
+        ("username","cpuUsage","timestamp")
     VALUES
-        (:OLD.username,:OLD.cpuUsage,:OLD.timestamp);
+        (:OLD."username",:OLD."cpuUsage",:OLD."timestamp");
 END;
