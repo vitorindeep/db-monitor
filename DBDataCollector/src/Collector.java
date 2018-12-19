07c388403cbf27cd74c0dc04f12665d46a46dde8
Vitor Castro <v.j.r.c1997@hotmail.com>
2018-12-19 19:35:56 +0000

    COLLECTOR working
    lacks history tables

--- DBDataCollector/src/Collector.java
+++ DBDataCollector/src/Collector.java
@@ -108,16 +108,18 @@ public class Collector  {
                         psmt.setFloat(5,Float.parseFloat(resultSet.getString("Used(MB)"))) ;
                         psmt.setFloat(6,Float.parseFloat(resultSet.getString("Max Ext(MB)"))) ;
                         psmt.setFloat(7,Float.parseFloat(resultSet.getString("% Free"))) ;
-                        psmt.executeUpdate() ;
-                        psmt.close() ;
+                        psmt.executeUpdate();
+                        psmt.close();
                     }
+                    monitorStmt.close();
                 }
                 System.out.println("$ TABLESPACES collected.");
 
 
                 // DATAFILES
                 System.out.println("$ DATAFILES start.");
-                String getDatafiles = "SELECT file_name, file_id, tablespace_name, bytes, blocks, status, autoextensible, maxbytes, maxblocks, online_status" +
+                String getDatafiles = "SELECT file_name, file_id, tablespace_name, bytes, blocks, status," +
+                                            " autoextensible, maxbytes, maxblocks, online_status" +
                                         " FROM dba_data_files" ;
                 resultSet = getStmt.executeQuery(getDatafiles);
                 while(resultSet.next()) {
@@ -127,11 +129,11 @@ public class Collector  {
                     String updateDatafiles = "UPDATE \"MONITOR\".\"DATAFILES\" " +
                             " SET \"bytes\" = " + Float.parseFloat(resultSet.getString("BYTES")) +
                             "," + " \"blocks\" = " + Float.parseFloat(resultSet.getString("BLOCKS")) +
-                            //"," + " \"status\" = " + resultSet.getString("STATUS") +
-                            //"," + " \"autoextensible\" = " + resultSet.getString("AUTOEXTENSIBLE") +
+                            "," + " \"status\" = " + "'" + resultSet.getString("STATUS") + "'" +
+                            "," + " \"autoextensible\" = " + "'" + resultSet.getString("AUTOEXTENSIBLE") + "'" +
                             "," + " \"maxbytes\" = " + Float.parseFloat(resultSet.getString("MAXBYTES")) +
                             "," + " \"maxblocks\" = " + Float.parseFloat(resultSet.getString("MAXBLOCKS")) +
-                            //"," + " \"onlinestatus\" = " + resultSet.getString("ONLINE_STATUS") +
+                            "," + " \"onlinestatus\" = " + "'" + resultSet.getString("ONLINE_STATUS") + "'" +
                             "," + " \"timestamp\" = CURRENT_TIMESTAMP" +
                             " WHERE \"filename\" = " + "'" + resultSet.getString("FILE_NAME") + "'";
 
@@ -142,11 +144,11 @@ public class Collector  {
 
                         String insertDatafiles = "INSERT INTO \"MONITOR\".\"DATAFILES\" "
                                 + "(\"filename\",\"fileid\",\"tablename\",\"bytes\",\"blocks\"," +
-                                //"\"status\",\"autoextensible\"," +
+                                "\"status\",\"autoextensible\"," +
                                 "\"maxbytes\",\"maxblocks\"," +
-                                //"\"onlinestatus\"," +
+                                "\"onlinestatus\"," +
                                 "\"timestamp\") "
-                                + "VALUES(?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)" ;
+                                + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)" ;
                         psmt = monitorConn.prepareStatement(insertDatafiles) ;
 
 
@@ -155,36 +157,38 @@ public class Collector  {
                         psmt.setString(3,resultSet.getString("TABLESPACE_NAME")) ;
                         psmt.setFloat(4,Float.parseFloat(resultSet.getString("BYTES"))) ;
                         psmt.setFloat(5,Float.parseFloat(resultSet.getString("BLOCKS"))) ;
-                        //psmt.setString(6,resultSet.getString("STATUS")) ;
-                        //psmt.setString(7,resultSet.getString("AUTOEXTENSIBLE")) ;
-                        psmt.setFloat(6,Float.parseFloat(resultSet.getString("MAXBYTES"))) ;
-                        psmt.setFloat(7,Float.parseFloat(resultSet.getString("MAXBLOCKS"))) ;
-                        //psmt.setString(10,resultSet.getString("ONLINE_STATUS")) ;
+                        psmt.setString(6,resultSet.getString("STATUS")) ;
+                        psmt.setString(7,resultSet.getString("AUTOEXTENSIBLE")) ;
+                        psmt.setFloat(8,Float.parseFloat(resultSet.getString("MAXBYTES"))) ;
+                        psmt.setFloat(9,Float.parseFloat(resultSet.getString("MAXBLOCKS"))) ;
+                        psmt.setString(10,resultSet.getString("ONLINE_STATUS")) ;
                         psmt.executeUpdate();
-                        psmt.close() ;
+                        psmt.close();
                     }
-
+                    stmt1.close();
                 }
                 System.out.println("$ DATAFILES collected.");
 
 
-                System.out.println("$ USERS start.");
+
                 // USERS
-                String users = "SELECT USERNAME, ACCOUNT_STATUS, COMMON, EXPIRY_DATE, DEFAULT_TABLESPACE, TEMPORARY_TABLESPACE, PROFILE, CREATED" +
-                        " FROM dba_users";
+                System.out.println("$ USERS start.");
+                String users = "SELECT USERNAME, ACCOUNT_STATUS, COMMON, EXPIRY_DATE, DEFAULT_TABLESPACE, TEMPORARY_TABLESPACE," +
+                                    " PROFILE, CREATED" +
+                                " FROM dba_users";
                 resultSet = getStmt.executeQuery(users);
 
                 while(resultSet.next()) {
 
                     Statement stmtUsers = monitorConn.createStatement();
                     String updateUser = " UPDATE \"MONITOR\".\"USERS\"" +
-                            " SET accStatus = " + "'" + resultSet.getString("ACCOUNT_STATUS")+"'"+
-                            "," + " common = " + resultSet.getString("COMMON") +
-                            "," + " defaultTablespace = " + resultSet.getString("DEFAULT_TABLESPACE") +
-                            "," + " tempTablespace = " + resultSet.getString("TEMPORARY_TABLESPACE") +
-                            "," + " profile = " + resultSet.getString("PROFILE") +
-                            "," + " timestamp = CURRENT_TIMESTAMP "  +
-                            " WHERE username = "  + resultSet.getString("USERNAME") ;
+                            " SET \"accStatus\" = " + "'" + resultSet.getString("ACCOUNT_STATUS")+"'"+
+                            "," + " \"common\" = " + "'" + resultSet.getString("COMMON") + "'" +
+                            "," + " \"defaultTablespace\" = " + "'" + resultSet.getString("DEFAULT_TABLESPACE") + "'" +
+                            "," + " \"tempTablespace\" = " + "'" + resultSet.getString("TEMPORARY_TABLESPACE") + "'" +
+                            "," + " \"profile\" = " + "'" + resultSet.getString("PROFILE") + "'" +
+                            "," + " \"timestamp\" = CURRENT_TIMESTAMP "  +
+                            " WHERE \"username\" = " + "'" + resultSet.getString("USERNAME") + "'" ;
 
                     int i=0;
                     i = stmtUsers.executeUpdate(updateUser);
@@ -192,8 +196,9 @@ public class Collector  {
                     if(i==0) {
 
                         users = "INSERT INTO \"MONITOR\".\"USERS\""
-                                + " (username,accStatus,common,expiryDate,defaultTablespace,tempTablespace,profile,created,timestamp)"
-                                + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP,?)" ;
+                                + " (\"username\",\"accStatus\",\"common\",\"expiryDate\",\"defaultTablespace\",\"tempTablespace\"," +
+                                "\"profile\",\"created\",\"timestamp\")"
+                                + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)" ;
                         psmt = monitorConn.prepareStatement(users) ;
 
                         psmt.setString(1,resultSet.getString("USERNAME")) ;
@@ -210,14 +215,15 @@ public class Collector  {
                         psmt.setDate(8,resultSet.getDate("CREATED")) ;
                         
                         psmt.executeUpdate();
-                        psmt.close() ;
+                        psmt.close();
                     }
+                    stmtUsers.close();
                 }
                 System.out.println("$ USERS collected.");
 
 
-                System.out.println("$ SGA start.");
                 // SGA
+                System.out.println("$ SGA start.");
                 String sga = "select name, value from v$sga";
 
                 resultSet = getStmt.executeQuery(sga);
@@ -226,86 +232,85 @@ public class Collector  {
 
                     Statement sgaStmt = monitorConn.createStatement();
                     String updateSga = " UPDATE \"MONITOR\".\"SGA\"" +
-                            " SET total = " + Float.parseFloat(resultSet.getString("VALUE")) +
-                            "," + " timestamp = CURRENT_TIMESTAMP "  +
-                            " WHERE name = "  + resultSet.getString("NAME");
-
+                            " SET \"total\" = " + Float.parseFloat(resultSet.getString("VALUE")) +
+                            "," + " \"timestamp\" = CURRENT_TIMESTAMP "  +
+                            " WHERE \"name\" = " + "'" + resultSet.getString("NAME") + "'";
 
                     int i=0;
                     i = sgaStmt.executeUpdate(updateSga);
 
                     if(i==0) {
 
-                        sga = "INSERT INTO \"MONITOR\".\"SGA\" (name,total,timestamp) "
+                        sga = "INSERT INTO \"MONITOR\".\"SGA\" (\"name\",\"total\",\"timestamp\") "
                                 + "VALUES(?, ?, CURRENT_TIMESTAMP)" ;
                         psmt = monitorConn.prepareStatement(sga) ;
 
                         psmt.setString(1,resultSet.getString("NAME")) ;
                         psmt.setFloat(2,Float.parseFloat(resultSet.getString("VALUE")));
                         psmt.executeUpdate();
+                        psmt.close();
                     }
-
+                    sgaStmt.close();
                 }
                 System.out.println("$ SGA collected.");
 
 
-                System.out.println("$ PGA start.");
                 // PGA
+                System.out.println("$ PGA start.");
                 String pga = "select name, value from v$sga";
-
                 resultSet = getStmt.executeQuery(pga);
 
                 while(resultSet.next()) {
 
                     Statement pgaStmt = monitorConn.createStatement();
                     String updatePga = " UPDATE \"MONITOR\".\"PGA\"" +
-                            " SET usedPga = " + Float.parseFloat(resultSet.getString("VALUE")) +
-                            "," + " timestamp = CURRENT_TIMESTAMP "  +
-                            " WHERE name = "  + resultSet.getString("NAME");
-
+                            " SET \"usedPga\" = " + Float.parseFloat(resultSet.getString("VALUE")) +
+                            "," + " \"timestamp\" = CURRENT_TIMESTAMP "  +
+                            " WHERE \"name\" = " + "'" + resultSet.getString("NAME") + "'";
 
                     int i=0;
                     i = pgaStmt.executeUpdate(updatePga);
 
                     if(i==0) {
 
-                        pga = "INSERT INTO \"MONITOR\".\"PGA\" (name,usedPga,timestamp) "
+                        pga = "INSERT INTO \"MONITOR\".\"PGA\" (\"name\",\"usedPga\",\"timestamp\") "
                                 + "VALUES(?, ?, CURRENT_TIMESTAMP)" ;
                         psmt = monitorConn.prepareStatement(pga) ;
 
                         psmt.setString(1,resultSet.getString("NAME")) ;
                         psmt.setFloat(2,Float.parseFloat(resultSet.getString("VALUE")));
                         psmt.executeUpdate();
+                        psmt.close();
                     }
-
+                    pgaStmt.close();
                 }
                 System.out.println("$ PGA collected.");
 
 
-                System.out.println("$ SESSIONS start.");
+
                 // SESSIONS
-                String ses = "select sid, username, status, server, schemaname, osuser, machine, port, type, logon_time from v$session" +
-                                " where username IS NOT NULL " ;
+                System.out.println("$ SESSIONS start.");
+                String ses = "select sid, username, status, server, schemaname, osuser," +
+                                    " machine, port, type, logon_time from v$session" +
+                            " where username IS NOT NULL " ;
 
                 resultSet = getStmt.executeQuery(ses);
 
                 while(resultSet.next()) {
-                    if(!resultSet.getString("USERNAME").equals("MANAGER")) {
 
                         Statement sesStmt = monitorConn.createStatement();
                         String updateSes = " UPDATE \"MONITOR\".\"SESSIONS\"" +
-                                " SET username = "+resultSet.getString("USERNAME")+
-                                "," + " status = " + resultSet.getString("STATUS") +
-                                "," + " server = " + resultSet.getString("SERVER") +
-                                "," + " schemaName = " + resultSet.getString("SCHEMANAME") +
-                                "," + " osUser = " + resultSet.getString("OSUSER") +
-                                "," + " machine = " + resultSet.getString("MACHINE") +
-                                "," + " port = " + resultSet.getString("PORT") +
-                                "," + " type = " + resultSet.getString("TYPE") +
-                                "," + " logonTime = " + resultSet.getDate("LOGON_TIME") +
-                                "," + " timestamp = CURRENT_TIMESTAMP "  +
-                                " WHERE sid = "  + resultSet.getString("SID");
-
+                                " SET \"username\" = " + "'" + resultSet.getString("USERNAME") + "'" +
+                                "," + " \"status\" = " + "'" + resultSet.getString("STATUS") + "'" +
+                                "," + " \"server\" = " + "'" + resultSet.getString("SERVER") + "'" +
+                                "," + " \"schemaName\" = " + "'" + resultSet.getString("SCHEMANAME") + "'" +
+                                "," + " \"osUser\" = " + "'" + resultSet.getString("OSUSER") + "'" +
+                                "," + " \"machine\" = " + "'" + resultSet.getString("MACHINE") + "'" +
+                                "," + " \"port\" = " + resultSet.getString("PORT") +
+                                "," + " \"type\" = " + "'" + resultSet.getString("TYPE") + "'" +
+                                "," + " \"logonTime\" = " + "'" + resultSet.getDate("LOGON_TIME") + "'" +
+                                "," + " \"timestamp\" = CURRENT_TIMESTAMP "  +
+                                " WHERE \"sid\" = "  + resultSet.getString("SID");
 
                         int i=0;
                         i = sesStmt.executeUpdate(updateSes);
@@ -313,7 +318,8 @@ public class Collector  {
                         if(i==0) {
 
                             ses = "INSERT INTO \"MONITOR\".\"SESSIONS\""
-                                    + " (sid,username,status,server,schemaName,osUser,machine,port,type,logonTime,timestamp)"
+                                    + " (\"sid\",\"username\",\"status\",\"server\",\"schemaName\",\"osUser\"," +
+                                    "\"machine\",\"port\",\"type\",\"logonTime\",\"timestamp\")"
                                     + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)" ;
                             psmt = monitorConn.prepareStatement(ses) ;
 
@@ -328,41 +334,51 @@ public class Collector  {
                             psmt.setString(9,resultSet.getString("TYPE")) ;
                             psmt.setDate(10,resultSet.getDate("LOGON_TIME")) ;
                             psmt.executeUpdate();
+                            psmt.close();
                         }
-                    }
+                    sesStmt.close();
                 }
                 System.out.println("$ SESSIONS collected.");
 
 
-                System.out.println("$ CPU start.");
-                // CPU
-                String cpu = "select name, value from v$sga";
 
+                // CPU
+                System.out.println("$ CPU start.");
+                String cpu = "SELECT USERNAME, SUM(CPU_USAGE) AS CPU_USAGE" +
+                            " FROM (SELECT se.username, ROUND (value/100) AS CPU_USAGE" +
+                                    " FROM v$session se, v$sesstat ss, v$statname st" +
+                                        " WHERE ss.statistic# = st.statistic#" +
+                                        " AND name LIKE  '%CPU used by this session%'" +
+                                        " AND se.sid = ss.SID" +
+                                        " AND se.username IS NOT NULL" +
+                                    " ORDER BY value DESC" +
+                            ")" +
+                            " GROUP BY USERNAME";
                 resultSet = getStmt.executeQuery(cpu);
 
                 while(resultSet.next()) {
 
                     Statement cpuStmt = monitorConn.createStatement();
                     String updateCpu = " UPDATE \"MONITOR\".\"CPU\"" +
-                            " SET cpuUsage = " + Float.parseFloat(resultSet.getString("CPU_USAGE")) +
-                            "," + " timestamp = CURRENT_TIMESTAMP "  +
-                            " WHERE username = "  + resultSet.getString("USERNAME");
-
+                            " SET \"cpuUsage\" = " + Float.parseFloat(resultSet.getString("CPU_USAGE")) +
+                            "," + " \"timestamp\" = CURRENT_TIMESTAMP "  +
+                            " WHERE \"username\" = " + "'" + resultSet.getString("USERNAME") + "'";
 
                     int i=0;
                     i = cpuStmt.executeUpdate(updateCpu);
 
                     if(i==0) {
 
-                        cpu = "INSERT INTO \"MONITOR\".\"CPU\" (username,cpuUsage,timestamp) "
+                        cpu = "INSERT INTO \"MONITOR\".\"CPU\" (\"username\",\"cpuUsage\",\"timestamp\") "
                                 + "VALUES(?, ?, CURRENT_TIMESTAMP)" ;
                         psmt = monitorConn.prepareStatement(cpu) ;
 
                         psmt.setString(1,resultSet.getString("USERNAME")) ;
                         psmt.setFloat(2,Float.parseFloat(resultSet.getString("CPU_USAGE")));
                         psmt.executeUpdate();
+                        psmt.close();
                     }
-
+                    cpuStmt.close();
                 }
                 System.out.println("$ CPU collected.");
 
